9969c8f445d14dd0e8f978a09576ac333d18a1df
Zeace <pern2500@ya.ru>
2016-11-04 15:09:09 +0500

    значительно переписал и оптимизировал

--- robbery.js
+++ robbery.js
@@ -2,19 +2,12 @@
 
 exports.isStar = true;
 var DAYS = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
-var timeForWork = {
-    mon: [{ 'dateFrom': new Date(2016, 9, 24), 'dateTo': new Date(2016, 9, 24) }],
-    tue: [{ 'dateFrom': new Date(2016, 9, 25), 'dateTo': new Date(2016, 9, 25) }],
-    wed: [{ 'dateFrom': new Date(2016, 9, 26), 'dateTo': new Date(2016, 9, 26) }]
-};
-var goodTime = [];
 
-exports.getAppropriateMoment = function ah(schedule, duration, workingHours) {
-    if (duration <= 0) {
-        goodTime = false;
-    } else {
-        goodTime = normalizeMinAndHour(findTime(schedule, duration, workingHours));
-    }
+exports.getAppropriateMoment = function (schedule, duration, workingHours) {
+    console.info(schedule, duration, workingHours);
+    var timeForWork = findTime(schedule, workingHours, duration);
+
+    var goodTime = normalizeMinAndHour(getTime(duration * 60000, timeForWork));
 
     return {
 
@@ -28,87 +21,38 @@ exports.getAppropriateMoment = function ah(schedule, duration, workingHours) {
 
                 return '';
             }
-
             if (typeof(goodTime) !== 'object' || duration === 0 || !template) {
 
                 return '';
             }
-            template = template.replace(/(%DD)/g, goodTime[2]);
-            template = template.replace(/(%HH)/g, goodTime[3]);
-            template = template.replace(/(%MM)/g, goodTime[4]);
+            template = template.replace(/(%DD)/g, goodTime[1]);
+            template = template.replace(/(%HH)/g, goodTime[2]);
+            template = template.replace(/(%MM)/g, goodTime[3]);
 
             return template;
         },
 
         tryLater: function () {
+            var oldGoodTime = goodTime;
             if (!this.exists()) {
 
                 return false;
             }
-            if (getTimeLater(duration * 60000)) {
+            if ((goodTime = getTimeLater(duration * 60000, timeForWork, goodTime))) {
 
                 return true;
             }
+            goodTime = oldGoodTime;
 
             return false;
         }
     };
 };
 
-function findTime(schedule, duration, workingHours) {
-    if (goodTime.length === 5) {
-        timeForWork = {
-            mon: [{ 'dateFrom': new Date(2016, 9, 24), 'dateTo': new Date(2016, 9, 24) }],
-            tue: [{ 'dateFrom': new Date(2016, 9, 25), 'dateTo': new Date(2016, 9, 25) }],
-            wed: [{ 'dateFrom': new Date(2016, 9, 26), 'dateTo': new Date(2016, 9, 26) }]
-        };
-        goodTime = [];
-    }
-    if (typeof(goodTime) === 'object' && goodTime.length === 0) {
-        if (workingHours.from === '' || !isValidBTime(workingHours)) {
+function findTime(schedule, workingHours) {
+    var workSchedule = addBankTime(workingHours);
 
-            return false;
-        }
-        if (!addBankTime(workingHours)) {
-
-            return false;
-        }
-        addSheduleTime(schedule, workingHours);
-    }
-
-    return getTime(duration * 60000);
-}
-
-function isValidBTime(workingHours) {
-    if (workingHours.to === '' || isValidOneTime(workingHours.to.split(':'))) {
-
-        return false;
-    }
-    if (workingHours.from === '' || isValidOneTime(workingHours.from.split(':'))) {
-
-        return false;
-    }
-
-    return true;
-}
-
-function isValidOneTime(time) {
-    var timeZone = parseInt(time[1].split('+')[1]);
-    time[0] = parseInt(time[0]);
-    time[1] = parseInt(time[1].split('+')[0]);
-    if (time[0] < 0 || time[0] > 23 || time[1] < 0 || time[1] > 59 || timeZone > 24) {
-
-        return true;
-    }
-
-    return false;
-}
-
-function getTimeZone(workingHours) {
-    var timeZoneBank = workingHours.from !== undefined ? Number(workingHours.from
-        .split('+')[1]) : 0;
-
-    return timeZoneBank;
+    return addSheduleTime(schedule, workingHours, workSchedule);
 }
 
 function addBankTime(workingHours) {
@@ -124,21 +68,28 @@ function addBankTime(workingHours) {
             return false;
         }
     }
-    addBankDays(dateArray);
 
-    return true;
+    return addBankDays(dateArray);
+}
+
+function getTimeZone(workingHours) {
+    var timeZoneBank = workingHours.from !== undefined ? Number(workingHours.from
+        .split('+')[1]) : 0;
+
+    return timeZoneBank;
 }
 
 function addBankDays(dateArray) {
-    timeForWork.mon[0].dateFrom = dateArray[0];
-    timeForWork.mon[0].dateTo = dateArray[3];
-    timeForWork.tue[0].dateFrom = dateArray[1];
-    timeForWork.tue[0].dateTo = dateArray[4];
-    timeForWork.wed[0].dateFrom = dateArray[2];
-    timeForWork.wed[0].dateTo = dateArray[5];
+    var bankSchedule = [];
+    for (var i = 0; i < 3; i++) {
+        bankSchedule.push({ 'dateFrom': dateArray[i], 'dateTo': dateArray[i + 3] });
+    }
+
+    return bankSchedule;
 }
 
-function addSheduleTime(schedule, workingHours) {
+function addSheduleTime(schedule, workingHours, oldWorkSchedule) {
+    var workSchedule = oldWorkSchedule;
     var keys = [];
     for (var key in schedule) {
         if ({}.hasOwnProperty.call(schedule, key)) {
@@ -148,11 +99,11 @@ function addSheduleTime(schedule, workingHours) {
     for (var j = 0; j < keys.length; j++) {
         for (var i = 0; i < schedule[keys[j]].length; i++) {
             correctionSchedule(schedule[keys[j]][i], getTimeZone(workingHours));
-            changeTimeForWork(schedule[keys[j]][i].from, schedule[keys[j]][i].to);
+            changeTimeForWork(schedule[keys[j]][i].from, schedule[keys[j]][i].to, workSchedule);
         }
     }
 
-    return true;
+    return workSchedule;
 }
 
 function correctionSchedule(interval, timeZone) {
@@ -192,27 +143,22 @@ function getDay(string) {
     }
 }
 
-function changeTimeForWork(from, to) {
+function changeTimeForWork(from, to, workSchedule) {
     if (from.getTime() === to.getTime() || from > to) {
 
         return false;
     }
-    for (var key in timeForWork) {
-        if (!({}.hasOwnProperty.call(timeForWork, key))) {
-            return false;
-        }
-        for (var i = 0; i < timeForWork[key].length; i++) {
-            calculateTime(from, to, key, i);
-        }
+    for (var i = 0; i < workSchedule.length; i++) {
+        calculateTime(from, to, i, workSchedule);
     }
 
     return true;
 }
 
-function calculateTime(from, to, key, i) {
+function calculateTime(from, to, i, workSchedule) {
     var set = 0;
-    var dateFrom = timeForWork[key][i].dateFrom;
-    var dateTo = timeForWork[key][i].dateTo;
+    var dateFrom = workSchedule[i].dateFrom;
+    var dateTo = workSchedule[i].dateTo;
     var newTime = [];
     if (from >= dateFrom && from <= dateTo) {
         newTime.push({ 'dateFrom': dateFrom, 'dateTo': from });
@@ -223,54 +169,41 @@ function calculateTime(from, to, key, i) {
         set++;
     }
     if (to > dateTo && from < dateFrom) {
-        changeTime(key, -2, dateFrom);
+        changeTime(-2, dateFrom, workSchedule);
 
         return true;
     }
     if (set !== 0) {
-        changeTime(key, i, newTime, set);
+        changeTime(i, newTime, set, workSchedule);
     }
 }
 
-function changeTime(key, i, newTime, set) {
+function changeTime(i, newTime, set, workSchedule) {
     if (i === -2) {
-        timeForWork[key].splice(0, timeForWork[key].length);
-        timeForWork[key].push({ 'dateFrom': newTime, 'dateTo': newTime });
+        workSchedule.splice(0, workSchedule.length);
+        workSchedule.push({ 'dateFrom': newTime, 'dateTo': newTime });
 
         return true;
     }
-    timeForWork[key].splice(i, 1);
+    workSchedule.splice(i, 1);
     for (var j = set; j > 0; j--) {
-        timeForWork[key].splice(i, 0, newTime[j - 1]);
+        workSchedule.splice(i, 0, newTime[j - 1]);
     }
 
     return true;
 }
 
-function getTime(duration) {
-    for (var key in timeForWork) {
-        if (!({}.hasOwnProperty.call(timeForWork, key))) {
+function getTime(duration, workSchedule) {
+    if (typeof (workSchedule) === 'undefined') {
 
-            return false;
-        }
-
-        if (calculateGetTime(key, duration)) {
-
-            return calculateGetTime(key, duration);
-        }
-
+        return false;
     }
-
-    return false;
-}
-function calculateGetTime(key, duration) {
-
-    for (var i = 0; i < timeForWork[key].length; i++) {
-        if ((timeForWork[key][i].dateTo - timeForWork[key][i].dateFrom) >= duration) {
-            var inform = [key, i];
-            inform.push(DAYS[timeForWork[key][i].dateTo.getDay() - 1]);
-            var time = [timeForWork[key][i].dateFrom.getHours()];
-            time.push(timeForWork[key][i].dateFrom.getMinutes());
+    for (var i = 0; i < workSchedule.length; i++) {
+        if ((workSchedule[i].dateTo - workSchedule[i].dateFrom) >= duration) {
+            var inform = [i];
+            inform.push(DAYS[workSchedule[i].dateTo.getDay() - 1]);
+            var time = [workSchedule[i].dateFrom.getHours()];
+            time.push(workSchedule[i].dateFrom.getMinutes());
             inform.push(time[0], time[1]);
 
             return inform;
@@ -285,69 +218,53 @@ function normalizeMinAndHour(array) {
 
         return false;
     }
-    var normalArray = [array[0], array[1], array[2]];
+    var normalArray = [array[0], array[1]];
+    if (array[2] < 10) {
+        normalArray.push('0' + array[2]);
+    } else {
+        normalArray.push(array[2]);
+    }
     if (array[3] < 10) {
         normalArray.push('0' + array[3]);
     } else {
         normalArray.push(array[3]);
     }
-    if (array[4] < 10) {
-        normalArray.push('0' + array[4]);
-    } else {
-        normalArray.push(array[4]);
-    }
 
     return normalArray;
 }
 
-function getTimeLater(duration) {
-    if (typeof(goodTime) === 'boolean' || goodTime.length !== 5) {
+function getTimeLater(duration, timeForWork, goodTime) {
+    if (typeof goodTime === 'undefined' || goodTime.length !== 4) {
 
         return false;
     }
-    var dateForCheck = timeForWork[goodTime[0]][goodTime[1]].dateFrom;
-    transferLate(dateForCheck, goodTime[0], goodTime[1]);
-    timeForWork[goodTime[0]][goodTime[1]].dateFrom.setMinutes(dateForCheck.getMinutes() + 30);
-    if (getTime(duration) === false) {
-        timeForWork[goodTime[0]][goodTime[1]].dateFrom = dateForCheck;
+    var dateForCheck = timeForWork[goodTime[0]].dateFrom;
+    var scheduleToTry = transferLate(dateForCheck, goodTime[0], timeForWork);
+    if (getTime(duration, scheduleToTry) === false) {
+        timeForWork[goodTime[0]].dateFrom = dateForCheck;
 
         return false;
     }
-    goodTime = normalizeMinAndHour(getTime(duration));
 
-    return getTime(duration);
+    return normalizeMinAndHour(getTime(duration, scheduleToTry));
 }
 
-function transferLate(dateForCheck, day, num) {
+function transferLate(dateForCheck, num, timeForWork) {
     var a = dateForCheck;
     var b = new Date(a);
     b.setMinutes(b.getMinutes() + 30);
-    if (timeForWork[day][num + 1] !== undefined && b > timeForWork[day][num + 1].dateFrom) {
-        addBusyLate(day, (num + 1), b.valueOf());
+    if (timeForWork[num + 1] !== undefined && b > timeForWork[num + 1].dateFrom) {
+        return addBusyLate((num + 1), b.valueOf(), timeForWork);
     }
-    if (a.getDay() !== b.getDay()) {
-        addBusyLate(getKey(b.getDay()), 0, b.valueOf());
-    }
+    timeForWork[num].dateFrom = b;
 
-    return a;
+    return timeForWork;
 }
 
-function getKey(day) {
-    switch (day) {
-        case 2:
-
-            return 'tue';
-        case 3:
-
-            return 'wed';
-        default:
-
-            return 0;
+function addBusyLate(num, value, timeForWork) {
+    if (timeForWork[num].dateFrom.valueOf() < value) {
+        timeForWork[num].dateFrom = new Date(value);
     }
-}
 
-function addBusyLate(day, num, value) {
-    if (timeForWork[day][num].dateFrom.valueOf() < value) {
-        timeForWork[day][num].dateFrom = new Date(value);
-    }
+    return timeForWork;
 }
